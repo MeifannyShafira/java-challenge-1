7d12df2a5a38c016f79a3324514131b3d8b9e7bd
Meifanny Azri Shafira <meifannyshafira@gmail.com>
2021-10-12 13:34:03 +0700

    first commit

---
+++ BasicJava.java
@@ -0,0 +1,22 @@
+public class BasicJava {
+    public static void main (String[] args){
+        //String firstName = "Meifanny";
+        //String middleName = "Azri";
+        //String lastName = "Shafira";
+        //int age = 21;
+        //String address = "Medan Dong";
+        //String phoneNumber = "085372189819";
+        //String hobby = "Swimming";
+        //String makananKesukaan = "Sate";
+        //String minumanKesukaan = "Air Putih";
+        System.out.println("first name :" + "Meifanny");
+        System.out.println("middle name :" + "Azri");
+        System.out.println("last name :" + "Shafira");
+        System.out.println("age:" + "21");
+        System.out.println("address:" + "Medan Dong");
+        System.out.println("phone number:" + "085372189819");
+        System.out.println("hobby:"+ "Swimming");
+        System.out.println("makanan Kesukaan:"+ "Sate");
+        System.out.println("minuman Kesukan:"+ "Air Putih");
+     }
+    }
---
+++ DataDiri.java
@@ -0,0 +1,24 @@
+public class DataDiri {
+    public static void main (String[] args){
+        String firstName = "Meifanny ";
+        String middleName = "Azri ";
+        String lastName = "Shafira ";
+        int age = 21;
+        String address = "Medan Dong ";
+        String phoneNumber = "085372189819 ";
+        String hobi = "berenang ";
+        String makananKesukaan = "sate padang ";
+        String minumanKesukaan = "Air Putih";
+
+        System.out.println("first name : " + firstName);
+        System.out.println("middleName:" + middleName);
+        System.out.println("last name : " + lastName);
+        System.out.println("Age : " + age);
+        System.out.println("address : " + address);
+        System.out.println("phoneNumber : " + phoneNumber);
+        System.out.println("hobi : " + hobi);
+        System.out.println("makanan kesukaan : " + makananKesukaan);
+        System.out.println("minuman Kesukaan: " + minumanKesukaan);
+        }
+    }   
+
